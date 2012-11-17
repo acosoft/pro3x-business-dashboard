cb542d22d330f7357c9fb4f7281e6a76a96bb980
Aleksandar Zgonjan <info@mali-zeleni.hr>
2012-11-17 14:10:19 +0100

    default styles replaced with jQueryUI Redmond theme

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Pro3x\SecurityBundle\Pro3xSecurityBundle(),
+            new Pro3x\jQueryUIBundle\Pro3xjQueryUIBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Pro3x/jQueryUIBundle/Pro3xjQueryUIBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pro3x\jQueryUIBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Pro3xjQueryUIBundle extends Bundle
+{
+}
