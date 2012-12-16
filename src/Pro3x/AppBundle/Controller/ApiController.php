f9d4df41e51496b5c0cf703a6fd212f829a6eb83
Aleksandar Zgonjan <info@mali-zeleni.hr>
2012-12-16 18:06:13 +0100

    api services, token generation

--- src/Pro3x/AppBundle/Controller/ApiController.php
+++ src/Pro3x/AppBundle/Controller/ApiController.php
@@ -11,7 +11,46 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  */
 class ApiController extends AdminController
 {
+	/**
+	 * 
+	 * @param \Pro3x\SecurityBundle\Entity\User $user
+	 * @return string
+	 */
+	private function getToken($user)
+	{
+		return $user->getId();
+	}
 	
+	public function decode($data)
+	{
+		return $data;
+	}
+	
+	public function encode($data)
+	{
+		return $data;
+	}
+	
+	/**
+	 * @Route("/login", name="api_login")
+	 */
+	public function loginAction()
+	{
+		$data = $this->decode($this->getRequest()->getContent());
+		
+		$username = $data->username;
+		$password = $data->password;
+		
+		$user = $this->getUserRepository()->findOneByName($username); /* @var $user \Pro3x\SecurityBundle\Entity\User */
+		
+		//TODO: exception handlin when user doesn't exist
+		
+		$response = array('fullName' => $user->getDisplayName(),
+			'cashRegister' => $position,
+			'token' => $this->getToken($user));
+		
+		return new \Symfony\Component\HttpFoundation\Response(json_encode($response));
+	}
 }
 
 ?>
